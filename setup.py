2fb5e23cfb7beede167e334058cbdc7b0216ecdd
Alexey Sudachen <alexey@sudachen.name>
2018-09-21 19:09:36 -0300

    updated

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-   name='codrv',
-   version='1.1',
-   description='Google Colab/Drive util functions',
+   name='gdrive',
+   version='1.5',
+   description='google colab GDrive interface',
    author='Alexey Sudachen',
    author_email='alexey.sudachen@vacasa.com',
-   packages=['codrv'],
+   packages=['gdrive'],
    install_requires=['pandas', 'singleton-decorator', 'pydrive', 'oauth2client'], 
 )
